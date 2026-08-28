b8cc92101d2d7c7ac1a6f0633ca9704f4d74f7e4
Chandirapriyan K <163757949+Chandruin64@users.noreply.github.com>
2026-08-28 10:35:28 +0530

    Create mst_sequencer.sv

---
+++ Master/mst_sequencer.sv
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------
+// Master Sequencer
+//------------------------------------------------------------------------------
+
+class mst_sequencer extends uvm_sequencer#(mst_xtn);
+
+    `uvm_component_utils(mst_sequencer)
+
+
+    //--------------------------------------------------------------------------
+    // Constructor
+    //--------------------------------------------------------------------------
+
+    function new(string name = "mst_sequencer",
+                 uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+endclass
